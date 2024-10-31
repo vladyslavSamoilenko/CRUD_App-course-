7119a5af59cca923793c3cf11af5ccafa790f8f9
dusha <dushasam26@gmail.com>
2024-11-01 00:23:42 +0100

    added deleteand update methods

--- src/main/java/com/samoilenko/crudApp/dao/PersonDAO.java
+++ src/main/java/com/samoilenko/crudApp/dao/PersonDAO.java
@@ -32,4 +32,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person person){
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(person.getName());
+    }
+
+    public void delete(int id){
+        people.removeIf(p -> p.getId() == id);
+    }
 }
