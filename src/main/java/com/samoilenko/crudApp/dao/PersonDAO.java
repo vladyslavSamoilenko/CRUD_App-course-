34e81df0454f23505bbb2523d1de30ab3628a586
dusha <dushasam26@gmail.com>
2024-11-01 01:16:15 +0100

    added validation

--- src/main/java/com/samoilenko/crudApp/dao/PersonDAO.java
+++ src/main/java/com/samoilenko/crudApp/dao/PersonDAO.java
@@ -13,10 +13,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 22, "tom@gmai.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 21, "bob@mail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 44, "mike@mail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 33, "katy@mail.com"));
     }
 
     public List<Person> index(){
@@ -36,6 +36,8 @@ public class PersonDAO {
     public void update(int id, Person person){
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(person.getName());
+        personToBeUpdated.setAge(person.getAge());
+        personToBeUpdated.setEmail(person.getEmail());
     }
 
     public void delete(int id){
